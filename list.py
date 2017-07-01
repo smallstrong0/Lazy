d90e7a566c330081a2bf2afff9d4935fdf617848
smallstrong0 <393019766@qq.com>
2017-07-01 12:10:03 +0800

    对文件夹下的隐藏文件进行规避

--- list.py
+++ list.py
@@ -13,8 +13,9 @@ def run():
 	f = open(os.getcwd()+"/name.txt", "wt") 
 	for root, dirs, files in os.walk(os.getcwd()+"/hdpi"):		
 		for name in files:
-			f.write(name+"\n")
-			print name
+			if (name[0] != "."):
+				f.write(name+"\n")
+				print name
 	f.close()
 	print '\033[1;32;40m'
 	print "请\n1.打开当前目录下的name.txt\n2.输入重新更改的名字(例子：demo.png=smallstrong)\n3.注意每一行更改一个文件的名字，更名后缀png或者jpg不需要加\n4.写完之后运行python cname.py命令进行文件更名"
